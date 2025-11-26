30c5f242d910902bbfc30966d893f01ee2316f81
Mahmoud Tarek <112757823+Mahmoudtarek70@users.noreply.github.com>
2025-11-26 04:19:03 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import { Mulish } from "next/font/google";
 import "./globals.css";
 import ViewCanvas from "@/components/ViewCanvas";
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
+import Header from "@/components/header";
+import Footer from "@/components/footer";
 
 const mulish = Mulish({
   subsets: ["cyrillic"],
